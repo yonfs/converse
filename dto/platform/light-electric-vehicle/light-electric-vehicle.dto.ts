7ef5d1e7c48bedee00a28fc7adfa3d25a149f136
seekhoo <hu@seekhoo.cn>
2025-09-26 11:17:24 +0800

    fix

--- dto/platform/light-electric-vehicle/light-electric-vehicle.dto.ts
+++ dto/platform/light-electric-vehicle/light-electric-vehicle.dto.ts
@@ -6,6 +6,10 @@ export type LightElectricVehicleDTO = {
     brandId: string | null
     // 型号ID
     modelId: string | null
+    // 品牌名称
+    brandName: string | null
+    // 型号名称
+    modelName: string | null
     // 创建时间
     createdAt: number
     // 创建人

6e343bfa2f8704fd964a21aae712f87f0b75fba0
Dalaor_XD <896408579@qq.com>
2025-10-14 11:43:18 +0800

    feat(bluetooth-accessory): 扩展蓝牙配件信息字段及关联数据

--- dto/platform/bluetooth-accessory/bluetooth-accessory.dto.ts
+++ dto/platform/bluetooth-accessory/bluetooth-accessory.dto.ts
@@ -1,11 +1,19 @@
 export type BluetoothAccessoryDTO = {
     id: string
-    // 设备型号
+    // 蓝牙仪表编号
+    bluetoothAccessoryNumber: string
+    // 蓝牙仪表名称
+    bluetoothAccessoryName: string
+    // 蓝牙仪表型号
     modelName: string
-    // 蓝牙地址
-    bluetoothAddress: string
+    // 安卓蓝牙地址
+    andriodAddress: string | null
+    // IOS蓝牙地址
+    iosAddress: string | null
+    // 商户id
+    merchantId: string | null
     // 商户名称
-    merchantName: string
+    merchantName: string | null
     // 创建时间
     createdAt: number
     // 更新时间

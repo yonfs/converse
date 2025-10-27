f19eed6cd3fbd61459c554672e10bf9c1f0fcf9f
zhtt <1683412300@qq.com>
2025-10-27 14:32:41 +0800

    permission

--- permission/merchant-permission.ts
+++ permission/merchant-permission.ts
@@ -1,6 +1,16 @@
 import type { PermissionModuleType } from './permission.types'
 
 export const MerchantPermissionModule: PermissionModuleType = [
+    // 车辆管理
+    {
+        id: 'lightEV',
+        items: ['lightEV.query']
+    },
+    // 蓝牙仪表管理
+    {
+        id: 'bluetoothAccessory',
+        items: ['bluetoothAccessory.query']
+    },
     // 账号管理
     {
         id: 'account',

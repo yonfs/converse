ae58f6b168cb7c82fcf24104f13682b343e8b13f
Dalaor_XD <896408579@qq.com>
2025-11-03 18:55:04 +0800

    refactor(dto): 删除 SimpleMerchantAccountRoleDTO 类型定义

--- dto/merchant/account-role/account-role.dto.ts
+++ dto/merchant/account-role/account-role.dto.ts
@@ -15,9 +15,3 @@ export type MerchantAccountRoleDTO = {
     /// 更新时间
     updatedAt: number | null
 }
-
-export type SimpleMerchantAccountRoleDTO = {
-    id: string
-    /// 名称
-    name: string
-}
